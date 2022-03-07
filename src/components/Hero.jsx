37262b82f0a2e8a982d2781c373699fa85613363
LucasFrazier <lucasfrazier1@gmail.com>
2022-03-07 09:03:29 -0500

    image related updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,7 @@ export default function Hero() {
         <h1 class="section__title section__title--intro">
             Hi, I am <strong>Jane Smith</strong>
         </h1>
-        <p class="section__subtitle section__subtitle--intro">front-end dev</p>
-        <img src="./images/dev-jane-01.jpg" alt="Jane Smith smiling" class="intro__img" />
+        <img src="./images/headshot512.png" alt="Lucas Frazier smiling" class="hero__img" />
     </section>
   )
 }
