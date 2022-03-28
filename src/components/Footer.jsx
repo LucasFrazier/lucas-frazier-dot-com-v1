4e3de9aded168fde2a0c5167f0089144d1076568
LucasFrazier <lucasfrazier1@gmail.com>
2022-03-28 16:44:28 -0400

    changes realted to font awesome icons and section titles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,12 @@ import './Footer.css'
 export default function Footer() {
   return (
     <footer class='footer'>
-        <a href='mailto:lucasfrazier1@gmail.com' class='footer__link'>contact@lucasfrazier.com</a>
         <ul class='social-list'>
+            <li class='social-list__item'>
+                <a class='social-list__link' href='mailto:lucasfrazier1@gmail.com'>
+                    <i class='fa fa-square-envelope'></i>
+                </a>
+            </li>
             <li class='social-list__item'>
                 <a class='social-list__link' href='https://linkedin.com/in/lucasfrazier/'>
                     <i class='fab fa-linkedin'></i>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,6 +43,17 @@ export default function Header() {
       <nav class='nav'>
         <ul class='nav__list'>
           {navLinkElements}
+          <li class='social-list__item'>
+            <a class='social-list__link' href='mailto:lucasfrazier1@gmail.com'>
+              <i class='fa fa-square-envelope'></i>
+            </a>
+            <a class='social-list__link' href='https://linkedin.com/in/lucasfrazier/'>
+              <i class='fab fa-linkedin'></i>
+            </a>
+            <a class='social-list__link' href='https://github.com/lucasfrazier'>
+              <i class='fab fa-github'></i>
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
